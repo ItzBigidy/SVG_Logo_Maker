248ffdc6df97b0d8fbc0597ae3088dd34bc0f6a8
ItzBigidy <adrianacosta@Adrians-MBP.attlocal.net>
2023-04-05 16:11:38 -0500

    imported modules into index.js, created SVG class and methods to render logo, created an array of questions using inquirer

---
+++ index.js
@@ -0,0 +1,44 @@
+const inquirer = requier('inquirer');
+const {Circle, Square, Triangle} = require('./lib/shapes');
+
+class Svg {
+    constuctor(){
+        this.textElement = '';
+        this.shapeElement = '':
+    }
+    render(){
+        return `<svg version="1.1"
+        width="300" height="200"
+        xmlns="http://www.w3.org/2000/svg">${this.shapeElement}${t}`
+    }
+    setTextElement(text, color){
+        this.textElement = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</text>` 
+    }
+    setShapeElement(shape){
+        this.shapeElement = shape.render();
+    }
+}
+
+const questions = [
+    {
+        type: 'input',
+        name: 'text',
+        message: 'TEXT: Enter up to 3 characters.',
+    },
+    {
+        type: 'input',
+        name: 'text-color',
+        message: 'TEXT COLOR: Enter a color keyword (OR a hexadecimal number).',
+    },
+    {
+        type: 'input',
+        name: 'shape-color',
+        message: 'SHAPE COLOR: Enter a color keyword (OR a hexadecimal number).',
+    },
+    {
+        type: 'list',
+        name: 'shape',
+        message: 'Select a background logo shape',
+        choices: ['Circle', 'Square', 'Triangle'],
+    },
+];
